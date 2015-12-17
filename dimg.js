88e5456e7f0dda4b22465a18ba3d0fff8e99a164
jonjohnjohnson <hi@jonjohnjohnson.com>
2015-12-17 14:26:43 -0500

    example setup that we will always copy to index for gh pages branch and fallback minimum dimg logic for when browsers like old ie dont return values correctly

--- dimg.js
+++ dimg.js
@@ -6,7 +6,7 @@ function dimg (imgEls, service) {
     }
   }
 
-  var pxDensity = window.devicePixelRatio;
+  var pxDensity = window.devicePixelRatio || 1;
   
   function createDynImg (img) {
     var imgWidth = img.dataset.dimgWidth || img.dataset.dimgW;
@@ -14,8 +14,24 @@ function dimg (imgEls, service) {
     var svgString = encodeURIComponent("<svg xmlns='http://www.w3.org/2000/svg' width='" + imgWidth + "' height='" + imgHeight + "'></svg>");
     img.src = "data:image/svg+xml," + svgString;
 
-    var renderedWidth  = Math.floor(img.clientWidth * pxDensity);
-    var renderedHeight = Math.floor(img.clientHeight * pxDensity);
+    var renderedWidth  = img.clientWidth * pxDensity;
+    var renderedHeight = img.clientHeight * pxDensity;
+    
+    if (!(renderedWidth > 0) || !(renderedHeight > 0)) {
+      if (imgWidth >= imgHeight) {
+        renderedWidth  = imgWidth/imgHeight;
+        renderedHeight = 1;
+      } else {
+        renderedWidth  = 1;
+        renderedHeight = imgWidth/imgHeight;
+      }
+      renderedWidth  *= 250;
+      renderedHeight *= 250;
+    }
+    
+    renderedWidth  = Math.floor(renderedWidth);
+    renderedHeight = Math.floor(renderedHeight);
+    
     var imageSource = img.dataset.dimg;
 
     img.src = service(imageSource,renderedWidth,renderedHeight);
